d41bde3d672e68369b302fac27356ef3520f1677
willamessantos <willamessantos@live.com>
2024-01-17 00:11:58 -0300

    feat: create scroll-up and scroll-bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,9 +31,40 @@ function scrollHeader() {
   if (this.scrollY >= 50) header.classList.add("scroll-header");
   else header.classList.remove("scroll-header");
 }
+
 window.addEventListener("scroll", scrollHeader);
 /*==================== SHOW SCROLL UP ====================*/
+function scrollup() {
+  const scrollup = document.getElementById("scroll-up");
+  if (this.scrollY >= 200) scrollup.classList.add("show-scroll");
+  else scrollup.classList.remove("show-scroll");
+}
+
+window.addEventListener("scroll", scrollup);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+
+window.addEventListener("scroll", scrollActive);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
